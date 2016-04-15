21858714b1b16b6592b447f077bc1bf3c1ba7cd8
livorov-v <v_livorov@mail.bg>
2016-04-15 22:45:10 +0300

    Added files via upload

---
+++ WithAndWithoutQuote.java
@@ -0,0 +1,9 @@
+/**
+ * Created by user on 12/15/2015.
+ */
+public class WithAndWithoutQuote {
+    public static void main (String[] args) {
+        System.out.println("Hello.");
+        System.out.println("\"Hello.\"");
+    }
+}
